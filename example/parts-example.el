2d8d3618cde0f3162c5bdbf989489f823be910c3
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-08-03 13:30:19 -0400

    Add parts example

---
+++ example/parts-example.el
@@ -0,0 +1,43 @@
+(require 'orm-table)
+
+(deftable part ()
+	  ((name :initarg :name
+		 :primary-key t
+		 :not-null t))
+	  :table parts)
+
+(deftable assembly ()
+	  ((name :initarg :name
+		 :primary-key t
+		 :not-null t))
+	  :table assemblies)
+
+(setq join-table (defassoc part :has-and-belongs-to-many assembly))
+
+(if (orm-created-p part)
+    (message "part already created")
+  (orm-create part))
+
+(if (orm-created-p assembly)
+    (message "assembly already created")
+  (orm-create assembly))
+
+(if (orm-created-p join-table)
+    (message "join-table already created")
+  (orm-create join-table))
+
+;; (orm-get part assemblies)
+(setq part-assemblies (car (orm-table-associations assembly)))
+
+(setq bolt (part :name "bolt" :assemblies (list "box"))
+      
+(setq box (assembly :name "box"))
+
+(orm-all (orm-get-assoc bolt 'assemblies))
+(orm-append (orm-get-assoc bolt 'assemblies) box)
+
+(orm-append (orm-get-assoc bolt 'assemblies) "box")
+
+
+(orm-all (orm-get-assoc bolt 'assemblies))
+;; (orm-all (orm-get part-assemblies)
